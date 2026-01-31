665be5ceb1be2230bad2f424439dbe9ccddf0146
barisercan <b.barisercan@gmail.com>
2026-01-31 14:33:50 +0300

    Initial Commit

---
+++ Pane/PaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaneApp.swift
+//  Pane
+//
+//  Created by Baha Baris Ercan on 31.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
